b494b8c77b55ca4ad3722ce1053fb6d7a88c1701
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-21 08:07:59 +0000

    Tidy and update version number

--- adminadjust.php
+++ adminadjust.php
@@ -17,25 +17,17 @@ class plgSystemAdminAdjust extends JPlugin
     protected $autoloadLanguage = true;
 
     /**
-     */
-    public function onUserAfterLogin($options)
-    {
-        JFactory::getApplication()->enqueueMessage('Testing', 'notice');
-        return true;
-    }
-    
-    /**
      * Add CSS and JS.
      */
     public function onBeforeRender()
-	{
+    {
         $app = JFactory::getApplication();
         if (!$app->isAdmin()) {
             return; // Only run in admin
         }
-		$document = JFactory::getDocument();
+        $document = JFactory::getDocument();
         $document->addStyleSheet('/css/admin-adjust.css');
         
         $document->addScript('/js/admin-adjust.js');
-	}
+    }
 }
