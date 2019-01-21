c4869598d9258a3f06e8ff9cacb1548710392b07
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-21 07:47:20 +0000

    Initial commit

---
+++ adminadjust.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.AdminAdjust
+ *
+ * @copyright   Copyright (C) NPE 2019.
+ * @license     MIT License; see LICENSE.md
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Tweaks to core admin screens.
+ */
+class plgSystemAdminAdjust extends JPlugin
+{
+    protected $autoloadLanguage = true;
+
+    /**
+     */
+    public function onUserAfterLogin($options)
+    {
+        JFactory::getApplication()->enqueueMessage('Testing', 'notice');
+        return true;
+    }
+    
+    /**
+     * Add CSS and JS.
+     */
+    public function onBeforeRender()
+	{
+        $app = JFactory::getApplication();
+        if (!$app->isAdmin()) {
+            return; // Only run in admin
+        }
+		$document = JFactory::getDocument();
+        $document->addStyleSheet('/css/admin-adjust.css');
+        
+        $document->addScript('/js/admin-adjust.js');
+	}
+}
