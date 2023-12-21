bfa2bf394bd062801d8b9c69c02a57219ba9333a
Andy Kirk <andy@andykirk.net>
2023-12-21 13:41:44 +0000

    J4

--- assets/js/admin-adjust.js
+++ assets/js/admin-adjust.js
@@ -1,4 +1,33 @@
+/*
+    iframe - fit height to contents.
+*/
+/*
 (function() {
+
+    var setIframeHeight = function(iframe) {
+
+        var newHeight = iframe.contentDocument.querySelector('html').offsetHeight;
+        iframe.style.height = newHeight + 'px';
+    };
+
+    var fit_content = function() {
+        // Get all elements we want to apply this to:
+        var elements = document.querySelectorAll('iframe');
+
+        Array.prototype.forEach.call(elements, function(el, i) {
+            var iframe        = el;
+            var iframe_window = iframe.contentWindow;
+
+            iframe.addEventListener('load', function() {
+                setIframeHeight(iframe);
+            });
+
+            iframe_window.addEventListener('resize', function(e) {
+                setIframeHeight(iframe);
+            });
+        });
+    };
+
     var ready = function(fn) {
         if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
             fn();
@@ -7,140 +36,6 @@
         }
     }
 
-	var admin_adjust = {
-
-        init: function() {
-            //var el = document.getElementById('folderFrame');
-            //admin_adjust.resizeIframe(el);
-
-
-            //jQuery(function(){
-            
-
-
-            jQuery(function(){
-
-                // Fix iFrame sizing:
-                var iFrames = jQuery('iframe');
-
-                function iResize() {
-                    for (var i = 0, j = iFrames.length; i < j; i++) {
-                        iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';
-                    }
-                }
-
-                if (jQuery.browser.safari || jQuery.browser.opera) {
-
-                    iFrames.load(function(){
-                        setTimeout(iResize, 0);
-                    });
-
-                    for (var i = 0, j = iFrames.length; i < j; i++) {
-                        var iSource = iFrames[i].src;
-                        iFrames[i].src = '';
-                        iFrames[i].src = iSource;
-                    }
-                } else {
-                    iFrames.load(function() {
-                        this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
-                    });
-                }
-
-
-                jQuery(window).resize(iResize);
-
-                // Poll for changes:
-                //window.setInterval(iResize, 1000);
-                
-                /*
-                // Add copyright control to media thumbnails:
-                jQuery('.manager.thumbnails.thumbnails-media').each(function(){
-                    
-                    var popover = [];
-                    popover.push('<p><small>Accepts <a href="https://www.markdownguide.org/basic-syntax/" target="_blank">Markdown</a></small></p>');
-                    popover.push('<p><textarea class="form-control  credit-input" rows="3"></textarea></p>');
-                    popover.push('<p>Preview:</p>');
-                    popover.push('<div class="credit-preview"></div>');
-                    popover.push('<p class="pull-right"><button type="button" class="btn btn-default  credit-cancel">Cancel</button> <button type="button" class="btn btn-primary  credit-ok">OK</button></p>');
-                    
-                    jQuery(this).find('.thumbnail:has(.imgThumb)').each(function(){
-                        var $thumb = jQuery(this);
-                        
-                        jQuery('<button class="btn btn-small btn-warning copyright-control">&copy;</button>')
-                        .prependTo($thumb)
-                        .webuiPopover({
-                            title:       'Attribution (Credit line)',
-                            content:     popover.join("\n"),
-                            closeable:   true,
-                            dismissible: true,
-                            cache:       false,
-                            onShow: function($element) {
-                                
-                                var $img    = $thumb.find('img');
-                                var src     = $img.attr('src').replace(window.location.origin, '');
-                                var src_b64 = btoa(src);
-                                
-                                //console.log(src);
-                                
-                                // Get the image credit line if there is one:
-                                jQuery.ajax({
-                                    url: '/plugins/system/tinker/ajax/get-image-info.php',
-                                    data: { 'image': src_b64 },
-                                    dataType: "json"
-                                })
-                                .done(function( response ) {
-                                    //console.log( response.data.copyright );
-                                    copyright = response.data.copyright;
-                                    
-                                    var converter      = new showdown.Converter(),
-                                        copyright_html = converter.makeHtml(copyright);
-                                    
-                                    $element.find('.credit-input')
-                                    .val(copyright)
-                                    .on('input', function(){
-                                        $element.find('.credit-preview')
-                                        .html(converter.makeHtml(jQuery(this).val()));
-                                    });
-                                    $element.find('.credit-preview')
-                                    .html(copyright_html);
-                                });
-                                
-                                
-                                
-                                $element.find('.credit-cancel').click(function(){
-                                    //console.log('Cancelled');
-                                    $element.hide();
-                                });
-                                
-                                $element.find('.credit-ok').click(function(){
-                                    copyright = $element.find('.credit-input').val();
-                                    //console.log('Okayed');
-                                    // Add/Update the credit line:
-                                    jQuery.ajax({
-                                        url: '/plugins/system/tinker/ajax/get-image-info.php?image=' + src_b64,
-                                        method: "POST",
-                                        data: { 'copyright': copyright }
-                                    })
-                                    .done(function( response ) {
-                                        console.log( response );
-                                    });
-                                    
-                                    $element.hide();
-                                });
-                            },
-                        });
-                    });
-                    
-                });*/
-                
-            });
-
-        },
-
-        /*resizeIframe: function(obj) {
-            obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-        }*/
-	}
-
-	ready(admin_adjust.init);
+    ready(fit_content);
 })();
+*/
---
+++ src/Extension/AdminAdjust.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.AdminAdjust
+ *
+ * @copyright   Copyright (C) NPEU 2023.
+ * @license     MIT License; see LICENSE.md
+ */
+
+namespace NPEU\Plugin\System\AdminAdjust\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
+
+/**
+ * Tweaks to core admin screens.
+ */
+class AdminAdjust extends CMSPlugin implements SubscriberInterface
+{
+    protected $autoloadLanguage = true;
+
+    /**
+     * An internal flag whether plugin should listen any event.
+     *
+     * @var bool
+     *
+     * @since   4.3.0
+     */
+    protected static $enabled = false;
+
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct($subject, array $config = [], bool $enabled = true)
+    {
+        // The above enabled parameter was taken from teh Guided Tour plugin but it ir always seems
+        // to be false so I'm not sure where this param is passed from. Overriding it for now.
+        $enabled = true;
+
+
+        #$this->loadLanguage();
+        $this->autoloadLanguage = $enabled;
+        self::$enabled          = $enabled;
+
+        parent::__construct($subject, $config);
+    }
+
+    /**
+     * function for getSubscribedEvents : new Joomla 4 feature
+     *
+     * @return array
+     *
+     * @since   4.3.0
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return self::$enabled ? [
+            'onAfterRender'        => 'onAfterRender',
+            'onBeforeRender'       => 'onBeforeRender',
+            'onContentPrepareForm' => 'onContentPrepareForm'
+        ] : [];
+    }
+
+
+    /**
+     * Replace strings in the body.
+     */
+    public function onAfterRender()
+    {
+        // Note this should be in it's own plugin, really.
+        $app = Factory::getApplication();
+
+        // Only in admin:
+        // I don't think this is necessary anymore (DEC 2023)
+        /*if ($app->isClient('administrator')) {
+            // New password fields should now use 'new-password' instead of 'off' for autocomplete.
+            // Get the response body.
+            $body = $app->getBody();
+
+            #$app->setBody(preg_replace('#(<input.*?type="password".*?)autocomplete="off"#s', '$1autocomplete="new-password"', $body));
+            #$app->setBody(str_replace('autocomplete="off" class="validate-password"', 'autocomplete="new-password" class="validate-password"', $body));
+        }*/
+
+        // Only in site:
+        if ($app->isClient('site')) {
+            // Replace all absolute local URLs with the correct ones:
+            if ($_SERVER['SERVER_NAME'] != 'www.npeu.ox.ac.uk') {
+                // Get the response body.
+                $body = $app->getBody();
+                $app->setBody(str_replace('https://www.npeu.ox.ac.uk', 'https://' . $_SERVER['SERVER_NAME'], $body));
+            }
+        }
+    }
+
+    /**
+     * Add CSS and JS.
+     */
+    public function onBeforeRender()
+    {
+        $app = Factory::getApplication();
+        if (!$app->isClient('administrator')) {
+            return; // Only run in admin
+        }
+        $document = Factory::getDocument();
+        #$document->addStyleSheet('/plugins/system/adminadjust/assets/css/admin-adjust.min.css');
+        $document->addStyleSheet('/plugins/system/adminadjust/assets/css/admin-adjust.css');
+        #document->addStyleSheet('https://cdn.jsdelivr.net/npm/webui-popover@1.2.18/dist/jquery.webui-popover.min.css');
+
+
+        #$document->addScript('https://cdn.jsdelivr.net/npm/webui-popover@1.2.18/dist/jquery.webui-popover.min.js');
+        #$document->addScript(' https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.0/showdown.min.js');
+        #$document->addScript('/plugins/system/adminadjust/assets/js/admin-adjust.js');
+    }
+
+    /**
+     * Prepare form and add my field.
+     *
+     * @param   Form  $form  The form to be altered.
+     * @param   mixed  $data  The associated data for the form.
+     *
+     * @return  boolean
+     *
+     * @since   <your version>
+     */
+    public function onContentPrepareForm(Event $event): void
+    {
+        [$form, $data] = array_values($event->getArguments());
+
+        if (!($form instanceof \Joomla\CMS\Form\Form)) {
+            $this->_subject->setError('JERROR_NOT_A_FORM');
+            return;
+        }
+
+        if (!($form instanceof \Joomla\CMS\Form\Form)) {
+            throw new GenericDataException(Text::_('JERROR_NOT_A_FORM'), 500);
+            return;
+        }
+
+        $app    = Factory::getApplication();
+        $option = $app->input->get('option');
+        FormHelper::addFormPath(dirname(dirname(__DIR__)) . '/forms');
+
+        switch($option)
+        {
+            case 'com_menus' :
+                if ($app->isClient('administrator') && isset($data->type) && $data->type == 'component') {
+                    $form->loadFile('menu_item', false);
+                }
+
+                return;
+
+            case 'com_modules' :
+                if ($app->isClient('administrator')) {
+                    $form->loadFile('module', false);
+                }
+
+                return;
+        }
+
+        return;
+    }
+}
